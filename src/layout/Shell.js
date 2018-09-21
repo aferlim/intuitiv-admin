6273959ca6d8be6144305ba2b07e11e79a18e3fc
André Ferreira Lima <andre.lima@grupoltm.com.br>
2018-09-21 18:27:04 -0300

    nav template :moyai:

--- src/layout/Shell.js
+++ src/layout/Shell.js
@@ -1,9 +1,13 @@
 import React from 'react'
 import Routes from '../config/Routes'
 import { Link } from 'react-router-dom'
+import Nav from '../Nav/Nav'
+
+import 'bootstrap/dist/css/bootstrap.min.css'
 
 const Shell = (props) => (
     <div>
+        <Nav />
         <h1>Shell</h1>
         <Link to="/dashboard" className="nav-link">Dashboard</Link>
         <Routes />
